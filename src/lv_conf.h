12cf1e307bd5db6b2be1a7c7fcd47fb845222f17
Stephen Brawner <brawner@gmail.com>
2025-03-06 14:59:33 -0800

    Reduce size

--- src/lv_conf.h
+++ src/lv_conf.h
@@ -80,7 +80,7 @@ typedef int16_t lv_coord_t;
 #define LV_MEM_CUSTOM      0
 #if LV_MEM_CUSTOM == 0
 /* Size of the memory used by `lv_mem_alloc` in bytes (>= 2kB)*/
-#  define LV_MEM_SIZE    (32U * 1000U)
+#  define LV_MEM_SIZE    (16U * 1000U)
 
 /* Complier prefix for a big array declaration */
 #include "esp_attr.h"
