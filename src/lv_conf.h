45cd4b4506363a0640e9341247f1877c80ca95ae
Stephen Brawner <brawner@gmail.com>
2025-03-06 14:54:49 -0800

    Try stack again

--- src/lv_conf.h
+++ src/lv_conf.h
@@ -77,13 +77,13 @@ typedef int16_t lv_coord_t;
  * The graphical objects and other related data are stored here. */
 
 /* 1: use custom malloc/free, 0: use the built-in `lv_mem_alloc` and `lv_mem_free` */
-#define LV_MEM_CUSTOM      1
+#define LV_MEM_CUSTOM      0
 #if LV_MEM_CUSTOM == 0
 /* Size of the memory used by `lv_mem_alloc` in bytes (>= 2kB)*/
-#  define LV_MEM_SIZE    (32U * 10000U)
+#  define LV_MEM_SIZE    (32U * 1000U)
 
 /* Complier prefix for a big array declaration */
-#  define LV_MEM_ATTR
+#  define LV_MEM_ATTR DRAM_ATTR
 
 /* Set an address for the memory pool instead of allocating it as an array.
  * Can be in external SRAM too. */
